4a7e31548df3183327aa1d5839c4acf0adfe5295
syahn <496300@naver.com>
2023-12-29 14:44:04 +0900

    feat: board 도메인 기능 구현

--- src/main/java/com/example/begame/domain/Board.java
+++ src/main/java/com/example/begame/domain/Board.java
@@ -1,5 +1,7 @@
 package com.example.begame.domain;
 
+import java.util.Arrays;
+
 public class Board {
     private int[][] board;
 
@@ -12,16 +14,28 @@ public class Board {
 
     //초기화 기능
     public void clear(){
+        Arrays.stream(board).map(b -> {
+            Arrays.fill(b, 0);
+            return b;
+        });
 
+        emptyNum = 19*19;
     }
 
     //돌 놓기
     public void update(int k, int x, int y){
-
+        board[x][y] = k;
     }
 
     //빈 공간 확인
     public boolean isFull(){
-        return false;
+        return emptyNum == 0;
+    }
+
+    //놓으려는 곳에 돌이 있는지 확인
+    public boolean isValidate(int x, int y){
+        if(board[x][y] != 0)
+            return false;
+        return true;
     }
 }
