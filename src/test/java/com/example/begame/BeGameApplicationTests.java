83675c2be28118456c19f88655ebd416d0724ba3
syahn <496300@naver.com>
2023-12-29 14:17:07 +0900

    init project

---
+++ src/test/java/com/example/begame/BeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.begame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
